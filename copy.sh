6ce869e0a43fd3b5aa37e5dd5832e60f62a5bd38
Andrew Eisenberg <aeisenberg@vmware.com>
2012-02-20 13:50:48 -0800

    changing copy script

--- copy.sh
+++ copy.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-cp ../org.eclipse.orion.client/bundles/org.eclipse.orion.client.core/web/plugins/esprimaPlugin/esprima.js plugins/esprimaPlugin/
-cp ../org.eclipse.orion.client/bundles/org.eclipse.orion.client.core/web/plugins/esprimaPlugin/esprimaJsContentAssist.js plugins/esprimaPlugin/
-cp ../org.eclipse.orion.client/bundles/org.eclipse.orion.client.core/web/plugins/esprimaPlugin/esprimaJsContentAssistPlugin.html plugins/esprimaPlugin/
+cp ../org.eclipse.orion.client/bundles/org.eclipse.orion.client.core/web/plugins/esprimaPlugin/esprima.js .
+cp ../org.eclipse.orion.client/bundles/org.eclipse.orion.client.core/web/plugins/esprimaPlugin/esprimaJsContentAssist.js .
+cp ../org.eclipse.orion.client/bundles/org.eclipse.orion.client.core/web/plugins/esprimaPlugin/esprimaJsContentAssistPlugin.html .
