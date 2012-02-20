e9c87481befacf59667b465d2d59f00e9380bee5
Andrew Eisenberg <aeisenberg@vmware.com>
2012-02-20 13:37:18 -0800

    copy over first go at plugin

---
+++ copy.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+cp ../org.eclipse.orion.client/bundles/org.eclipse.orion.client.core/web/plugins/esprimaPlugin/esprima.js plugins/esprimaPlugin/
+cp ../org.eclipse.orion.client/bundles/org.eclipse.orion.client.core/web/plugins/esprimaPlugin/esprimaJsContentAssist.js plugins/esprimaPlugin/
+cp ../org.eclipse.orion.client/bundles/org.eclipse.orion.client.core/web/plugins/esprimaPlugin/esprimaJsContentAssistPlugin.html plugins/esprimaPlugin/
